1ff00c552b550ac5e24be882d0d937f161f9be1c
Prithwish Nandi <prithwishnp.n@gmail.com>
2022-04-05 02:19:51 +0530

    feedback app add

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,18 +1,47 @@
 import React, { useState } from 'react'
+import RatingSelect from './RatingSelect'
 import Button from './shared/Button'
 import Card from './shared/Card'
 
-function FeedbackForm() {
+function FeedbackForm({ handleAdd }) {
 
     const [text, setText] = useState('')
+    const [rating, setRating] = useState(10)
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
-    const handleTextChange = (e) => {
-        setText(e.target.value)
-    }
+    const handleTextChange = ({target: {value}}) => { 
+        if (value === '') {
+          setBtnDisabled(true)
+          setMessage(null)
+        } else if (value !== '' && value.trim().length < 10) { 
+          setMessage('Text must be at least 10 characters')
+          setBtnDisabled(true)
+        } else {
+          setMessage(null)
+          setBtnDisabled(false)
+        }
+        setText(value)
+      }
+
+      const handleSubmit = (e) => {
+        e.preventDefault()
+        if(text.trim().length >= 10) {
+            const newFeedback = {
+                text,
+                rating
+            }
+
+            handleAdd(newFeedback)
+
+            setText('')
+        }
+      }
 
   return <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
           <h2>How would you rate your service with us?</h2>
+          <RatingSelect select={(rating) => setRating(rating)} />
           <div className="input-group">
               <input
               onChange={handleTextChange} 
@@ -20,8 +49,10 @@ function FeedbackForm() {
               placeholder='Write a review' 
               value={text}    
               />
-              <Button type="submit">Send</Button>
+              <Button type="submit" isDisabled={btnDisabled}>Send</Button>
           </div>
+
+          {message && <div className='message'>{message}</div>}
       </form>
   </Card>
 }
